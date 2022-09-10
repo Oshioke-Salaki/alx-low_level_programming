6ed122ba49604dd3cbc03a3768e91b2393fccf7e
Oshioke-Salaki <salaki1902@gmail.com>
2022-09-09 17:25:14 -0700

    r

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,38 +3,25 @@
 
 
 /**
- * main - prints all possible combinations of single-digit numbers
- * Return: Always 0 (Success)
+ * main
+ * return 0
  */
 
 int main(void)
-
 {
+	int n;
 
-		int n;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(',');
 
-
-
-			for (n = 48; n < 58; n++)
-
-					{
-
-								putchar(n);
-
-										if (n != 57)
-
-													{
-
-																	putchar(',');
-
-																				putchar(' ');
-
-																						}
-
-											}
-
-				putchar('\n');
-
-					return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 
 }
