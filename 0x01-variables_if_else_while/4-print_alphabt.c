59dc8695e05c65b6cdc70f86a899b8f8d0862125
Oshioke-Salaki <salaki1902@gmail.com>
2022-09-09 17:50:58 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints
+ * followed by
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
