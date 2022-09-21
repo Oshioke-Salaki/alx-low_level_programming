60587fddc5ab1458a42685e9620d684add4af83c
Oshioke-Salaki <salaki1902@gmail.com>
2022-09-21 15:01:31 -0700

    j

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies
+ * @dest: des
+ * @src: source
+ * @n: number
+ *
+ * Return: pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
